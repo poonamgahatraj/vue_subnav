4dc3d1976397dfebab3d77b5a37e2aad193ce5f8
Poonam Gahatraj <poonamgtrz@gmail.com>
2023-10-12 18:18:22 +0545

    vue_subnav

--- vue_subnav/src/components/navbar.vue
+++ vue_subnav/src/components/navbar.vue
@@ -1,13 +1,26 @@
 <template>
     <div class="nav">
-        <a>Home</a>
-        <button class="btn" @click="Showsubnav1">About</button>
-        <div class="btn-content" v-if="shownav1">
+        <div>
+            <button class="btn1">Home</button>
+        <button class="btn" @mouseover="shownav1=true"  @mouseleave="shownav1 = false">About</button>
+        
+        <button class="btn" @mouseover="shownav2=true"  @mouseleave="shownav2 = false"  >Services</button>
+       
+        <button class="btn" @mouseover="shownav3=true"  @mouseleave="shownav3 = false">Partner</button>
+
+        <button class="btn1" >Contact</button>
+
+        
+        
+        
+        </div>
+       
+    </div>
+    <div class="btn-content" v-if="shownav1">
             <a >Company</a>
             <a >Team</a>
             <a >Careers</a>
         </div>
-        <button class="btn" @click="Showsubnav2">Services</button>
 
         <div class="btn-content" v-if="shownav2">
             <a>Bring</a>
@@ -15,7 +28,6 @@
             <a >Package</a>
             <a >Express</a>
         </div>
-        <button class="btn" @click="Showsubnav3">Partner</button>
 
         <div class="btn-content" v-if="shownav3">
             <a >Link 1</a>
@@ -23,8 +35,6 @@
             <a >Link 3</a>
             <a >Link 4</a>
         </div>
-        <a>Contact</a>
-    </div>
 </template>
 
 <script>
@@ -34,28 +44,20 @@ export default{
             shownav1 : false,
             shownav2 : false,
             shownav3 : false
+           
         }
     },
-    methods:{
-        Showsubnav1(){
-            this.shownav1=!this.shownav1
-        },
-        Showsubnav2(){
-            this.shownav2=!this.shownav2
-        },
-        Showsubnav3(){
-            this.shownav3=!this.shownav3
-        }
-    }
+    
 }
 </script>
 
 <style>
 .nav{
     background-color: #333; 
-    padding:10px;
+   
     color:white;
-    padding:10px;
+    height: 40px;
+    
 }
 
 .btn{
@@ -63,6 +65,22 @@ export default{
     border:none;
     color:white;
     margin-right: 10px;
+    font-size: 15px;
+    height: 40px;
+}
+
+.btn1{
+    background-color: #333; 
+    border:none;
+    color:white;
+    margin-right: 10px;
+    font-size: 15px;
+    height: 40px;
+}
+
+.btn1:hover{
+    background:red;
+    
 }
 
 .btn:hover{
@@ -74,8 +92,12 @@ a{
     margin-right: 10px;
 }
 
+a:hover{
+    background:red;
+}
 .btn-content{
-    padding: 10px;
+    padding: 20px;
     background: red;
+    
 }
 </style>
